cabb799b305b85d7a7b749df61e2977f157af818
samitkapoor <samitkapoor77@gmail.com>
2022-02-06 17:41:46 +0530

    Median in a row wise sorted matrix

---
+++ Geeksforgeeks/Median in a row-wise sorted Matrix/main.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    int median(vector<vector<int>> &matrix, int r, int c)
+    {
+        // code here
+        int size = r * c;
+        vector<int> array(size);
+        int x = 0;
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                array[x++] = matrix[i][j];
+            }
+        }
+
+        sort(array.begin(), array.end());
+
+        int median = array.size() / 2;
+
+        return array[median];
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for (int i = 0; i < r; ++i)
+            for (int j = 0; j < c; ++j)
+                cin >> matrix[i][j];
+        Solution obj;
+        cout << obj.median(matrix, r, c) << endl;
+    }
+    return 0;
+} // } Driver Code Ends
