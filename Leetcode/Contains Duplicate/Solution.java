7d5ff9d3be4b3ab6c3689d3949e7c0532a33943f
samitkapoor <samitkapoor77@gmail.com>
2022-05-16 12:16:31 +0530

    Contains Duplicate - Java Solution

---
+++ Leetcode/Contains Duplicate/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        
+        for(int i=1 ; i<nums.length ; i++){
+            if(nums[i-1] == nums[i]) return true;
+        }
+        
+        return false;
+    }
+}
