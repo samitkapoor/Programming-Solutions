32ff1312b402f676961fb81adcdd8cf0a88db744
samitkapoor <samitkapoor77@gmail.com>
2022-05-17 22:10:48 +0530

    Palindrome Linked List - Java Solution

---
+++ Leetcode/Palindrome Linked List/Solution.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static ListNode reverseLL(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode temp = reverseLL(slow);
+
+        while (temp != null) {
+            if (temp.val != head.val)
+                return false;
+            temp = temp.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+}
