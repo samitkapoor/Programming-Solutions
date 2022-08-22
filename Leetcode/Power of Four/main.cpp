d276ecac6c2f9560cc5e4b1c24c9c5baf272835f
Samit Kapoor <77121931+samitkapoor@users.noreply.github.com>
2022-08-22 22:48:23 +0530

    Power of Four

---
+++ Leetcode/Power of Four/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        
+        if(n == 0) return false;
+        
+        while(n % 4 == 0){
+            n /= 4;
+        }
+        
+        if( n == 1 ) return true;
+        else return false;
+        
+    }
+};
