912868f99efe78e771d3adffcbbe387ad28aa88e
Samit Kapoor <77121931+samitkapoor@users.noreply.github.com>
2022-08-24 17:07:17 +0530

    Power of Three

---
+++ Leetcode/Power of Three/main.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n == 0) return false;
+        
+        while(n % 3 == 0){
+            n = n/3;
+        }
+        
+        if(n == 1) return true;
+        else return false;
+    }
+}
