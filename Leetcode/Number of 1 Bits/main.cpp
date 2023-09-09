72e37d181b07134b44b0af8b465282cce84cb09c
Samit Kapoor <77121931+samitkapoor@users.noreply.github.com>
2023-09-10 01:55:20 +0530

    Number of 1 Bits

---
+++ Leetcode/Number of 1 Bits/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        for(int i = 0 ; i < 32 ; i++){
+            if(n & 1 != 0) ans++;
+
+            n = n >> 1;
+        }
+
+        return ans;
+    }
+};
