22b02e0cb23ca6ea9e1cb06bf3cde8a9c01989e7
Samit Kapoor <77121931+samitkapoor@users.noreply.github.com>
2024-02-06 19:41:21 +0530

    Group Anagrams

---
+++ Leetcode/Group Anagrams/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, int> mp;
+        vector<vector<string>> ans; 
+
+        for (int i = 0; i < strs.size(); i++) {
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            if (mp.find(temp) != mp.end()) {
+                ans[mp[temp]].push_back(strs[i]);
+            } else {
+                mp[temp] = ans.size();
+                ans.push_back({strs[i]});
+                
+            }
+        }
+
+        return ans;
+    }
+};
+
