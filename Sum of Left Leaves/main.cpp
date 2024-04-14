7f7a6043242b32ffebad7445b2e5698961c38f43
Samit Kapoor <77121931+samitkapoor@users.noreply.github.com>
2024-04-14 20:12:26 +0530

    Sum of Left Leaves

---
+++ Sum of Left Leaves/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if(node->left != nullptr) {
+                if(node->left->left == nullptr && node->left->right == nullptr)
+                    sum += node->left->val;
+                q.push(node->left);
+            }
+            if(node->right != nullptr) {
+                q.push(node->right);
+            }
+        }
+
+        return sum;
+    }
+};  
